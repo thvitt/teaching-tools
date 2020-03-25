087525c49c222c35729d358655c12f157ab5ca1d
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2020-03-25 11:56:28 +0100

    mailmerge: Preview options

--- mailmerge.py
+++ mailmerge.py
@@ -16,6 +16,9 @@ import sys
 
 from io import BytesIO
 
+from more_itertools import first
+from tabulate import tabulate
+
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                  description="Simple Mass-Mailer")
 parser.add_argument('template', type=argparse.FileType(),
@@ -30,6 +33,8 @@ parser.add_argument('-H', '--html', action='store_true', default=False,
                     help="ensure we send UTF-8 encoded HTML")
 parser.add_argument('-n', '--dry-run', action='store_true', default=False,
                     help="Don't actually send")
+parser.add_argument('-t', '--to', nargs='?', const='from',
+                    help="Only generate the first e-mail, and send it to the given address (default: the From: address)")
 parser.add_argument('-s', '--smtp-server', nargs=1, default="mailmaster.uni-wuerzburg.de",
                     help="SMTP server to use. Only SSL (port 465) supported.")
 parser.add_argument('-u', '--smtp-user', nargs=1, default='thv76an',
@@ -38,14 +43,31 @@ parser.add_argument('-u', '--smtp-user', nargs=1, default='thv76an',
 options = parser.parse_args()
 template = options.template.read()
 reader = DictReader(options.csv)
+records = list(reader)
 
+print("Records:")
+print(tabulate(records))
+
+template_vars = {v.value for v in pymustache.compiled(template).children if v.type_string == 'V'}
+columns = set(reader.fieldnames)
+
+if template_vars - columns:
+    print("ERROR: The following template variables are not in the table:", ", ".join(template_vars - columns))
+if columns - template_vars:
+    print("INFO: The following table columns are not in the template:", ", ".join(columns - template_vars))
+
+answer = input("Continue? [y, N] ").strip().lower()
+if answer != "y":
+    sys.exit(1)
 
 server = smtplib.SMTP_SSL(options.smtp_server)
 
 if not options.dry_run:
     server.login(options.smtp_user, getpass())
 
-for row in reader:
+if options.to:
+    reader = records[0]
+for row in records:
     formatted = pymustache.render(template, row)
     header, body = formatted.split('\n\n', maxsplit=1)
     msg = Parser(policy=policy.EmailPolicy(utf8=True)).parsestr(header, headersonly=True)
@@ -54,7 +76,13 @@ for row in reader:
     else:
         msg.set_content(body, charset='utf-8')
 
-    recipients = msg.get_all('to', []) + msg.get_all('cc', []) + msg.get_all('bcc', [])
+    if options.to:
+        if options.to == 'from':
+            recipients = msg.get_all('from', [])
+        else:
+            recipients = [options.to]
+    else:
+        recipients = msg.get_all('to', []) + msg.get_all('cc', []) + msg.get_all('bcc', [])
     addr = getaddresses([r for r in recipients if r])
     print("Sende an:", addr)
 
