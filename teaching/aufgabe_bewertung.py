94049e8390cff2c063f46717345be25f6a35de53
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2024-04-11 18:29:47 +0200

    aufgabe_bewertung: some enhancements
    - extract: unpack zip file
    - sort headers in Bewertung

--- teaching/aufgabe_bewertung.py
+++ teaching/aufgabe_bewertung.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+from importlib.util import module_from_spec
 from operator import attrgetter
 from pathlib import Path
+import shutil
 from tempfile import mkstemp
 from subprocess import run
 from typing import List, Dict
 
 from blessings import Terminal
 from more_itertools import one
+from zipfile import ZipFile
+
 try:
     from readchar import key, readkey
 except ImportError as e:
@@ -16,21 +20,20 @@ import sys
 import re
 import csv
 
-GRADE_FIELD = 'Bewertung'
+GRADE_FIELD = "Bewertung"
 
-NAME_FIELD = 'Vollständiger Name'
+NAME_FIELD = "Vollständiger Name"
 
-COMMENT_FIELD = 'Feedback als Kommentar'
+COMMENT_FIELD = "Feedback als Kommentar"
 
 filename = sys.argv[1] if len(sys.argv) > 1 else "Bewertung.md"
 t = Terminal()
 
-GRADE = re.compile('^([0-9,.]+(/[0-9]+)?|b|nb)\s*$')
-NAME = re.compile('^## ([^_\n]+)(_.*)?')
+GRADE = re.compile(r"^([0-9,.]+(/[0-9]+)?|b|nb)\s*$")
+NAME = re.compile(r"^## ([^_\n]+)(_.*)?")
 
 
 class Bewertung:
-
     def __init__(self, name, lines=None, common_note=None):
         self.name = name
         self.lines = [] if lines is None else lines
@@ -61,8 +64,8 @@ class Bewertung:
     def text(self):
         lines = self.lines
         if self.common_note:
-            lines += ['', '----', ''] + self.common_note
-        return '\n'.join(lines)
+            lines += ["", "----", ""] + self.common_note
+        return "\n".join(lines)
 
     @property
     def lastname(self):
@@ -72,17 +75,26 @@ class Bewertung:
         if self.filename:
             return  # already done
 
-        fd, self.filename = mkstemp(suffix='.md', prefix='bewertung', text=True)
-        with open(fd, mode="w", encoding='utf-8') as f:
+        fd, self.filename = mkstemp(suffix=".md", prefix="bewertung", text=True)
+        with open(fd, mode="w", encoding="utf-8") as f:
             f.writelines(self.lines)
 
     def copy(self):
         self.mktemp()
-        run("pandoc -t html -f markdown -i '{}' | xclip -i -selection clipboard -t text/html"
-            .format(self.filename), shell=True)
+        run(
+            "pandoc -t html -f markdown -i '{}' | xclip -i -selection clipboard -t text/html".format(
+                self.filename
+            ),
+            shell=True,
+        )
 
     def to_html(self):
-        pandoc = run(["pandoc", "-t", "html", "-f", "markdown"], input=self.text, text=True, capture_output=True)
+        pandoc = run(
+            ["pandoc", "-t", "html", "-f", "markdown"],
+            input=self.text,
+            text=True,
+            capture_output=True,
+        )
         return pandoc.stdout
 
     def close(self):
@@ -91,11 +103,7 @@ class Bewertung:
             self.filename = None
 
     def __str__(self):
-        return (self.name
-                + "=" * len(self.name)
-                + "\n\n"
-                + "".join(self.lines)
-                + "\n\n")
+        return self.name + "=" * len(self.name) + "\n\n" + "".join(self.lines) + "\n\n"
 
 
 def read(filename):
@@ -109,16 +117,16 @@ def read(filename):
         Liste von :class:`Bewertung`en
     """
     bewertungen = []
-    text = '\n' + Path(filename).read_text()
-    parts = text.split('\n## ')
+    text = "\n" + Path(filename).read_text()
+    parts = text.split("\n## ")
     preamble = parts[0].strip()
-    
+
     for section in parts[1:]:
-        lines = section.strip().split('\n')
-        names = lines[0].split('; ')
+        lines = section.strip().split("\n")
+        names = lines[0].split("; ")
         for name in names:
             bewertungen.append(Bewertung(name, lines[1:], common_note=preamble))
-    
+
     return bewertungen
 
 
@@ -126,14 +134,16 @@ def prepare_input_file(filename, moodle_csv=None):
     with open(filename, "wt", encoding="utf-8") as file:
         if moodle_csv:
             with moodle_csv.open() as f:
-                reader = csv.DictReader(f)
+                reader = sorted(
+                    csv.DictReader(f), key=lambda row: row.get("Vollständiger Name", "")
+                )
                 for row in reader:
-                    if 'abgegeben' in row['Status']:
+                    if "abgegeben" in row["Status"]:
                         file.write(f"## {row['Vollständiger Name']}\n\n")
-                    if 'spät' in row['Status']:
+                    if "spät" in row["Status"]:
                         file.write(f"* {row['Status']}\n\n")
         else:
-            names = {fn.split('_')[0] for fn in os.listdir()}
+            names = {fn.split("_")[0] for fn in os.listdir()}
             lines = sorted(["## {}\n\n".format(name) for name in names])
             file.writelines(lines)
 
@@ -141,10 +151,29 @@ def prepare_input_file(filename, moodle_csv=None):
     sys.exit(0)
 
 
+def unzip_file(archive_, target_):
+    archive, target = map(Path, (archive_, target_))
+    moodle_csv = archive.with_name("Bewertungen-" + archive.stem + ".csv")
+    target.mkdir(parents=True, exist_ok=True)
+    zipfile = ZipFile(archive)
+    for entry in zipfile.infolist():
+        file = Path(zipfile.extract(entry, target))
+        if file.suffix == ".zip":
+            subzip_target = file.parent / file.name
+            subzip_target.mkdir(exist_ok=True, parents=True)
+            ZipFile(file).extractall(subzip_target)
+    shutil.copy2(moodle_csv, target)
+    os.chdir(target)
+    prepare_input_file("Bewertung.md", moodle_csv)
+
+
 def main():
     options = getargparser().parse_args()
-    if options.markdown_file.exists():
-        bewertungen = sorted(read(options.markdown_file), key=attrgetter('name'))
+    if options.extract:
+        unzip_file(*options.extract)
+        return
+    elif options.markdown_file.exists():
+        bewertungen = sorted(read(options.markdown_file), key=attrgetter("name"))
     else:
         prepare_input_file(options.markdown_file, options.moodle_csv)
         return
@@ -155,7 +184,9 @@ def main():
         pass  # b/nb -> kein Durchschnitt
 
     if options.moodle_csv:
-        augment_moodle_csv(bewertungen, options.moodle_csv, options.markdown_file.with_suffix('.csv'))
+        augment_moodle_csv(
+            bewertungen, options.moodle_csv, options.markdown_file.with_suffix(".csv")
+        )
 
     if options.interactive:
         display_bewertungen(bewertungen)
@@ -167,15 +198,22 @@ def display_bewertungen(bewertungen):
         bewertung = bewertungen[index]
         try:
             bewertung.copy()
-            print(t.bold(bewertung.name), "\t", "Bewertung:", t.red(str(bewertung.grade)))
-            print(bewertung.text, '\n')
+            print(
+                t.bold(bewertung.name), "\t", "Bewertung:", t.red(str(bewertung.grade))
+            )
+            print(bewertung.text, "\n")
 
             prompt = True
             while prompt:
-                print(t.green("#{}/{}:".format(index + 1, len(bewertungen))),
-                      "{} ({})".format(bewertung.name, bewertung.grade),
-                      "↑ {t.bold}p{t.normal}revious, ↓⏎ {t.bold}n{t.normal}ext, {t.bold}c{t.normal}opy again, {t.bold}q{t.normal}uit"
-                      .format(t=t), end=' > ', flush=True)
+                print(
+                    t.green("#{}/{}:".format(index + 1, len(bewertungen))),
+                    "{} ({})".format(bewertung.name, bewertung.grade),
+                    "↑ {t.bold}p{t.normal}revious, ↓⏎ {t.bold}n{t.normal}ext, {t.bold}c{t.normal}opy again, {t.bold}q{t.normal}uit".format(
+                        t=t
+                    ),
+                    end=" > ",
+                    flush=True,
+                )
                 ch = readkey()
                 if ch in ["p", "P", key.UP, key.BACKSPACE]:
                     print("previous")
@@ -196,7 +234,9 @@ def display_bewertungen(bewertungen):
             bewertung.close()
 
 
-def augment_moodle_csv(bewertungen: List[Bewertung], moodle_csv: Path, output: Path = None):
+def augment_moodle_csv(
+    bewertungen: List[Bewertung], moodle_csv: Path, output: Path = None
+):
     by_name: Dict[str, Bewertung] = {bew.name: bew for bew in bewertungen}
 
     with moodle_csv.open() as infile:
@@ -205,14 +245,16 @@ def augment_moodle_csv(bewertungen: List[Bewertung], moodle_csv: Path, output: P
         fieldnames = list(reader.fieldnames)
 
     if COMMENT_FIELD not in fieldnames:
-        print('Unter Aufgabe / Feedback [x] Feedback als Kommentar ankreuzen und Bewertungtabelle neu exportieren')
+        print(
+            "Unter Aufgabe / Feedback [x] Feedback als Kommentar ankreuzen und Bewertungtabelle neu exportieren"
+        )
         fieldnames.append(COMMENT_FIELD)
 
     if output is None:
-        moodle_csv.replace(moodle_csv.with_suffix(moodle_csv.suffix + '~'))
+        moodle_csv.replace(moodle_csv.with_suffix(moodle_csv.suffix + "~"))
         output = moodle_csv
 
-    with output.open('wt') as outfile:
+    with output.open("wt") as outfile:
         writer = csv.DictWriter(outfile, fieldnames)
         writer.writeheader()
         found = set()
@@ -222,20 +264,23 @@ def augment_moodle_csv(bewertungen: List[Bewertung], moodle_csv: Path, output: P
             if name in by_name:
                 line[GRADE_FIELD] = by_name[name].grade
                 line[COMMENT_FIELD] = by_name[name].to_html()
-                if 'Status' in fieldnames:
-                    line['Status'] = 'Freigegeben'
-                if 'Bewerter/in' in fieldnames:
-                    line['Bewerter/in'] = 'Thorsten Vitt'
+                if "Status" in fieldnames:
+                    line["Status"] = "Freigegeben"
+                if "Bewerter/in" in fieldnames:
+                    line["Bewerter/in"] = "Thorsten Vitt"
                 found.add(name)
             else:
                 not_found.add(name)
             writer.writerow(line)
 
-    print(f'{len(found)} Kursteilnehmer wurden bewertet, {len(not_found)} nicht.')
-    if not_found: print(' - fehlend:', ', '.join(not_found))
+    print(f"{len(found)} Kursteilnehmer wurden bewertet, {len(not_found)} nicht.")
+    if not_found:
+        print(" - fehlend:", ", ".join(not_found))
     missed = set(by_name) - found
     if missed:
-        print(f'!!! {len(missed)} Bewertete sind nicht der Kurstabelle: {", ".join(missed)}')
+        print(
+            f'!!! {len(missed)} Bewertete sind nicht der Kurstabelle: {", ".join(missed)}'
+        )
 
 
 def export_simple_csv(bewertungen, outfile):
@@ -247,15 +292,18 @@ def export_simple_csv(bewertungen, outfile):
 
 
 def getargparser():
-    default_md = Path('Bewertung.md')
+    default_md = Path("Bewertung.md")
     default_csv = None
-    csvs = Path().glob('*.csv')
-    foreign_csvs = set(csvs) - {default_md.with_suffix('.csv')}
+    csvs = Path().glob("*.csv")
+    foreign_csvs = set(csvs) - {default_md.with_suffix(".csv")}
     if len(foreign_csvs) == 1:
         default_csv = one(foreign_csvs)
 
     import argparse
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,description="""
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="""
     Tool für einen Offline-Bewertungsworkflow.
     
     Die Grundlage ist eine Markdown-Datei (Bewertung.md), in der für jede_n Student_in
@@ -266,17 +314,35 @@ def getargparser():
     in eine Bewertungstabelle, wie sie aus Moodle / WueCampus kommt, eintragen oder 
     pro Teilnehmer_in in die Zwischenablage kopieren. Es kann auch eine leere Vorlage 
     erzeugen.
-    """)
-    parser.add_argument('markdown_file', default=default_md, type=Path, nargs='?',
-                        help="Markdown-Datei fürs Feedback. Ein '##'-Abschnitt pro Student_in, "
-                             "Zeile mit nur der Punktzahl für die Bewertung."
-                             "Existiert die Datei nicht, wird sie erzeugt und das Skript endet.")
-    parser.add_argument('-m', '--moodle-csv', type=Path, nargs='?', default=default_csv,
-                        help="Bewertungsexportdatei aus Moodle")
-    parser.add_argument('-i', '--interactive', action='store_true',
-                        help="die Bewertungsergebnisse interaktiv präsentieren und kopieren")
+    """,
+    )
+    parser.add_argument(
+        "markdown_file",
+        default=default_md,
+        type=Path,
+        nargs="?",
+        help="Markdown-Datei fürs Feedback. Ein '##'-Abschnitt pro Student_in, "
+        "Zeile mit nur der Punktzahl für die Bewertung."
+        "Existiert die Datei nicht, wird sie erzeugt und das Skript endet.",
+    )
+    parser.add_argument(
+        "-m",
+        "--moodle-csv",
+        type=Path,
+        nargs="?",
+        default=default_csv,
+        help="Bewertungsexportdatei aus Moodle",
+    )
+    parser.add_argument(
+        "-i",
+        "--interactive",
+        action="store_true",
+        help="die Bewertungsergebnisse interaktiv präsentieren und kopieren",
+    )
+
+    parser.add_argument("-x", "--extract", nargs=2, help="Abgaben extrahieren")
     return parser
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
