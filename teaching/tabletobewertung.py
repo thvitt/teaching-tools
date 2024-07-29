1f5b9a148463b8a4fcfb0afd2a32928328171163
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2024-07-29 14:44:24 +0200

    Better feedback for tabletobewertung

--- teaching/tabletobewertung.py
+++ teaching/tabletobewertung.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
 
+from collections import UserDict
 from itertools import chain
 from pathlib import Path
-from typing import Annotated, Literal, Optional, Union
+from typing import Annotated, Optional
 
 import pandas as pd
 import pymustache
 from typer import Argument, Typer
+import logging
+
+logger = logging.getLogger(__name__)
 
 app = Typer()
 
 
-def complete_table():
-    return [
-        str(p)
-        for p in chain(
-            Path().glob("*.csv"),
-            Path().glob("*.tsv"),
-            Path().glob("*.xlsx"),
-            Path().glob("*.ods"),
-        )
-    ]
+class LoggingDict(UserDict):
+
+    def __contains__(self, key: object) -> bool:
+        contained = super().__contains__(key)
+        if not contained:
+            logger.error("Template variable %s not found in data", key)
+        return contained
+
+
+def glob_completer(*patterns):
+    def completer():
+        return list(map(str, chain.from_iterable(Path().glob(p) for p in patterns)))
+
+    return completer
 
 
 @app.command()
@@ -29,15 +37,21 @@ def convert(
         Path,
         Argument(
             help="Table with source data",
-            autocompletion=complete_table,
+            autocompletion=glob_completer("*.csv", "*.tsv", "*.xlsx", "*.ods"),
             exists=True,
         ),
     ],
     template_file: Annotated[
-        Path, Argument(help="Mustache template file for each row", exists=True)
+        Path,
+        Argument(
+            help="Mustache template file for each row",
+            exists=True,
+            autocompletion=glob_completer("*.md", "*.tmpl"),
+        ),
     ],
     output: Annotated[Optional[Path], Argument(help="Output file")] = None,
 ):
+    logging.basicConfig(level=logging.INFO)
     if table.suffix == ".csv":
         data = pd.read_csv(table)
     elif table.suffix == ".tsv":
@@ -50,11 +64,12 @@ def convert(
         raise ValueError(
             f"Unknown input file format: {table.suffix}. Please provide a CSV, TSV, XLSX, or ODS file.",
         )
-
+    data = data.dropna(subset=data.columns[0])
+    logger.info("Data table:\n%s", data)
     template = template_file.read_text()
     result = []
     for _, row in data.iterrows():
-        result.append(pymustache.render(template, row))
+        result.append(pymustache.render(template, LoggingDict(row.to_dict())))
     result_text = "\n".join(result)
     if output is None:
         print(result_text)
